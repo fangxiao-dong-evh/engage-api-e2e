2daa0f5e6e4184e616afb0a27ee6f5747088079b
Fangxiao Dong <fdong@evolenthealth.com>
2019-04-10 16:25:01 -0400

    use require

--- test/sms.test.js
+++ test/sms.test.js
@@ -1,7 +1,9 @@
 'use strict';
 
-import test from 'ava';
+const {pfyHttps} = require('../lib/promisifiedhttps');
+const test = require('ava');
 
-test('foo bar is foo bar', t => {
-    
+test('foo bar is foo bar', async t => {
+    t.is(1,1);
+    await pfyHttps({});
 });
