248c3dc4206765fea7f649a7fc19b70febb6bd6c
Fangxiao Dong <fdong@evolenthealth.com>
2019-04-10 21:04:34 -0400

    add assertion

--- test/sms.test.js
+++ test/sms.test.js
@@ -1,9 +1,20 @@
 'use strict';
 
-const {pfyHttps} = require('../lib/promisifiedhttps');
+const pfyHttps = require('../lib/promisifiedhttps');
 const test = require('ava');
 
-test('foo bar is foo bar', async t => {
-    t.is(1,1);
-    await pfyHttps({});
+test.skip('iOS - Redirect to iTune', async t => {
+    let result = await pfyHttps({path: 'appstoreredirect', isIOS: false});
+    
+    t.is(result.statusCode, 302);
+
+    t.is(result.responseHeaders.Location, 'https://play.google.com/store/apps/details?id=com.myidentifi.engage');
+});
+
+test('Android - Redirect to Google Play', async t => {
+    let result = await pfyHttps({path: 'appstoreredirect', isIOS: true});
+    
+    t.is(result.statusCode, 302);
+
+    t.is(result.responseHeaders.location, 'https://itunes.apple.com/us/app/identifi-engage/id1239920478?mt=8');
 });
